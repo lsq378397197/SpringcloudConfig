9c31a5c6a34454ca27d1736ac1c9ec1e3afb2f45
liushangqing <378397197@qq.com>
2017-12-12 09:28:42 +0800

    测试提交

---
+++ eureka-server2/src/main/java/com/lsq/EurekaServer2Application.java
@@ -0,0 +1,17 @@
+package com.lsq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author liushangqing
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServer2Application.class, args);
+	}
+}
